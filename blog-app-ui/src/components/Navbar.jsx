293c4ffd834e798f4470188624e7a92b9b35fd1f
Masood Acheampong <masoodacheampong@gmail.com>
2024-02-21 22:13:58 +0000

    Finished implementing a blog app interface setup.

---
+++ blog-app-ui/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Logo } from "../assets/svgs";
+
+function Navbar() {
+  return (
+    <div className="">
+      {/* Logo */}
+      <div className="">
+        <img src={Logo} alt="" />
+      </div>
+      {/* List */}
+      <div></div>
+    </div>
+  );
+}
+
+export default Navbar;
